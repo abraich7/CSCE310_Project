1e29d90db5f9900997ea7f2624b3502a21bd4cf3
Jacob Parker <parkerjacob37@gmail.com>
2023-12-05 18:39:16 -0600

    fixed user type casing and added initial navbar for links pages

--- login/student_links.php
+++ login/student_links.php
@@ -1,10 +1,11 @@
 <?php
     include_once '../includes/dbh.inc.php';
+    include_once '../includes/navbar.php';
 
     // confirm user is a student
     session_start();
 
-    if (!isset($_SESSION['user_type']) || $_SESSION['user_type'] !== 'Student') {
+    if (!isset($_SESSION['user_type']) || $_SESSION['user_type'] !== 'student') {
         // Redirect to login page or display error message
         header("Location: login.php"); // Redirect to login page
         exit();
